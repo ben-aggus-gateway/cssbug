2f144d9ba613dbfe2913065c98cde37e81d2aff7
ben-aggus-gateway <ben.aggus@gatewayfirst.com>
2023-11-15 11:18:43 -0600

    bug demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,19 @@
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <div
+        className={`text-white 
+py-6 place-items-center 
+grid md:grid-cols-12 
+md:gap-7 grid-cols-8 
+gap-2 bg-blue-500`}
+      >
+        <span className="text-xl font-semibold md:col-start-2 md:col-span-10 col-span-6 col-start-2">
+          Test Content
+        </span>
+      </div>
+    </div>
+  );
 }
